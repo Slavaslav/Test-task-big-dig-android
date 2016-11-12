a919e12ffc91a707b6d1caaf4171dbc73c6a0bd2
awecea <awecea@gmail.com>
2016-11-12 13:38:15 +0200

    Add HistoryFragment. Add TestFragment. Add TabLayout. Add ViewPager.

--- app/src/main/java/com/myapplication/testtaskbigdig1/MainActivity.java
+++ app/src/main/java/com/myapplication/testtaskbigdig1/MainActivity.java
@@ -1,13 +1,62 @@
 package com.myapplication.testtaskbigdig1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements HistoryFragment.OnFragmentInteractionListener, TestFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addPage(new TestFragment(), "Test");
+        adapter.addPage(new HistoryFragment(), "History");
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        ArrayList<String> pageNames = new ArrayList<>();
+
+        public void addPage(Fragment fragment, String pageName) {
+            fragments.add(fragment);
+            pageNames.add(pageName);
+        }
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return pageNames.get(position);
+        }
     }
 }
