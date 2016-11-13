5a4953b6c560a9ef53e3bb35b4a575f4f97b227a
awecea <awecea@gmail.com>
2016-11-13 18:36:35 +0200

    add pass value between applications

--- app/src/main/java/com/myapplication/testtaskbigdig1/TestFragment.java
+++ app/src/main/java/com/myapplication/testtaskbigdig1/TestFragment.java
@@ -33,12 +33,12 @@ public class TestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_test, container, false);
-        EditText editText = (EditText) view.findViewById(R.id.edit_text_image_uri);
+        final EditText editText = (EditText) view.findViewById(R.id.edit_text_image_uri);
         Button buttonOk = (Button) view.findViewById(R.id.button_ok);
         buttonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startTaskBigDig2();
+                startTaskBigDig2(editText.getText().toString());
             }
         });
         return view;
@@ -64,12 +64,14 @@ public class TestFragment extends Fragment {
     public interface OnFragmentInteractionListener {
     }
 
-    private void startTaskBigDig2() {
+    private void startTaskBigDig2(String editText) {
         Context ctx = getActivity();
         try {
             Intent i = ctx.getPackageManager().getLaunchIntentForPackage(getString(R.string.name_package_application_b));
             if (i == null)
                 throw new PackageManager.NameNotFoundException();
+            i.setAction(Intent.ACTION_SEND);
+            i.putExtra(Intent.EXTRA_TEXT, editText);
             startActivity(i);
         } catch (PackageManager.NameNotFoundException e) {
             Toast toast = Toast.makeText(ctx, R.string.application_b_not_found, Toast.LENGTH_SHORT);
--- testtaskbigdig2/src/main/java/com/myapplication/testtaskbigdig2/MainActivity.java
+++ testtaskbigdig2/src/main/java/com/myapplication/testtaskbigdig2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myapplication.testtaskbigdig2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        handleStartup();
+    }
+
+    private void handleStartup() {
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (Intent.ACTION_SEND.equals(action)) {
+            String uri = intent.getStringExtra(Intent.EXTRA_TEXT);
+            if (uri != null) {
+                // add to database
+            }
+        } else {
+            // autoclose
+        }
     }
 }
